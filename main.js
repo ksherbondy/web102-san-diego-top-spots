5b47d8b239fd84173712e76af4e040834a9671ac
Kristopher Sherbondy <kristophersherbondy@Kristophers-MacBook-Air.local>
2026-03-16 17:07:30 -0500

    Feat: Added in all JS logic, configured server, and set up page.

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
     // write your code here
+    $.getJSON("data.json", function(data) {
+        console.log(data);
+        data.forEach(function(spot) {
+            const latitude = spot.location[0];
+            const longitude = spot.location[1];
+            const mapURL = 'https://www.google.com/maps?q=' + latitude + ',' + longitude;
+
+            const row = `
+                <tr>
+                    <td>${spot.name}</td>
+                    <td>${spot.description}</td>
+                    <td>
+                        <a href="${mapURL}" target="_blank">
+                            <button>View on Map</button>
+                        </a>
+                    </td>
+                </tr>
+            `;
+            $('tbody').append(row);
+        });
+    });
 
 });
